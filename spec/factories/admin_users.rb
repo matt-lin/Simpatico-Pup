555928f408c83a20abe20b240ff709a60454b6b5
Alex Ho <hogunghiu@msn.com>
2017-07-07 20:14:44 +0000

    <Debug> created sample model for admin_user

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :admin_user do
-    
+    email "admin@berkeley.edu"
+    password "password"
+    password_confirmation "password"
+    remember_me true
   end
 
 end
