b56775322132e2345b226cd47db05f03841586a6
Alex Ho <hogunghiu@msn.com>
2017-07-10 22:35:13 +0000

    <REFACTOR> Refactoring pup_step.rb

--- features/step_definitions/web_step.rb
+++ features/step_definitions/web_step.rb
@@ -1,22 +1,29 @@
+require 'uri'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+
+module WithinHelpers
+  def with_scope(locator)
+    locator ? within(*selector_for(locator)) { yield } : yield
+  end
+end
+World(WithinHelpers)
+
+When /^I go to "(.*)"$/ do |link|
+  find("#"+link).click
+end
+
 When(/^(?:admin )go to (.+)$/) do |page_name|
     visit "/admin/"+page_name
+end
+
+Given /^I am on (.+)/ do |page_name|
+	visit path_to(page_name)
 end
 
 When /^I reload the page$/ do
   visit [ current_path, page.driver.request.env['QUERY_STRING'] ].reject(&:blank?).join('?')
 end
-
-
-# require 'uri'
-# require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
-# require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
-
-# module WithinHelpers
-#   def with_scope(locator)
-#     locator ? within(*selector_for(locator)) { yield } : yield
-#   end
-# end
-# World(WithinHelpers)
 
 # # Single-line step scoper
 # When /^(.*) within (.*[^:])$/ do |step_name, parent|
@@ -28,9 +35,9 @@ end
 #   with_scope(parent) { step "#{step_name}:", table_or_string }
 # end
 
-# Given /^(?:I )am on (.+)$/ do |page_name|
-#   visit path_to(page_name)
-# end
+Given /^(?:I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
 
 # # When /^(?:I )go to (.+)$/ do |page_name|
 # #   visit path_to(page_name)
