4acdfba3e03d90550b7f0217ccc56364cb7fd74b
Gilbert Lo <lhygilbert@berkeley.edu>
2017-07-10 22:36:50 +0000

    <Development> Finish admin delete subscriber feature

--- app/admin/newsletter_user.rb
+++ app/admin/newsletter_user.rb
@@ -1,11 +1,16 @@
 ActiveAdmin.register NewsletterUser do
   filter :email
   menu :label => 'Newsletter Subscribers'
-
+  
+  actions :all, except: [:update, :show, :edit, :new]
+  
   index do
+    selectable_column
+    
     column :id
     column :email
     column :created_at
+    actions
   end
 
 end
