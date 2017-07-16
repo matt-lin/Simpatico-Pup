9d6f66718f64ec081c8672f56c971415f0faac4d
Alex Ho <hogunghiu@msn.com>
2017-07-16 20:26:52 +0000

    <Test> Created test for model: text.rb

--- spec/models/text_spec.rb
+++ spec/models/text_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
     @text =  FactoryGirl.create(:text, :title => 'Testing', :body => 'body')
     
     it 'returns title field' do
-      expect(Text.find_by_name('Testing').body).to include 'body'
+      expect(Text.find_by_name('Testing').body).to eq 'body'
     end
   end
   
