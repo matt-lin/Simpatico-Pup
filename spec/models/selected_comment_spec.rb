b1564a4fa8da483fec0d4afa6cce56ff0f2a6004
Licong Wang <lcw@berkeley.edu>
2017-07-16 04:15:56 +0000

    <Test> Added rspec test for selected_comment

--- spec/models/selected_comment_spec.rb
+++ spec/models/selected_comment_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe SelectedComment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "select comments from selected comments" do
+    before :each do
+      @comment1 = FactoryGirl.create(:selected_comment, :content => 'good dog')
+      @comment2 = FactoryGirl.create(:selected_comment, :content => 'bad dog')
+    end
+    
+    it "select a comment randomly from all selected comments" do
+        chosen_comment = SelectedComment.find_randomly
+        expect(@comment1.content == chosen_comment.content || @comment2.content == chosen_comment.content).to be true
+    end
+  end
 end
