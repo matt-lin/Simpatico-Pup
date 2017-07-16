8bb567b3bdf4c1712489e0f48c26210edbc39374
Licong Wang <lcw@berkeley.edu>
2017-07-16 20:11:14 +0000

    Current

--- spec/models/selected_comment_spec.rb
+++ spec/models/selected_comment_spec.rb
@@ -1,22 +1,6 @@
 require 'spec_helper'
 
 describe SelectedComment do
-<<<<<<< HEAD
-<<<<<<< HEAD
-  describe "select comments from selected comments" do
-    before :each do
-      @comment1 = FactoryGirl.create(:selected_comment, :content => 'good dog')
-      @comment2 = FactoryGirl.create(:selected_comment, :content => 'bad dog')
-    end
-    
-    it "select a comment randomly from all selected comments" do
-        chosen_comment = SelectedComment.find_randomly
-        expect(@comment1.content == chosen_comment.content || @comment2.content == chosen_comment.content).to be true
-    end
-  end
-=======
-=======
->>>>>>> 5eee49e0d4ebe94e5c570a40b64d9b8e348bb131
     describe "find_randomly should return a random selected_comment" do
         before :each do 
             @comment1 = FactoryGirl.create(:selected_comment, :content => "comment1")
@@ -63,8 +47,4 @@ describe SelectedComment do
     it "find_randomly should return nil if no selected_comment" do
         expect(SelectedComment.find_randomly).to eq nil
     end
-<<<<<<< HEAD
->>>>>>> a4ac70bdc6ff7ed75fd8b1a4108e1383d1da6c1e
-=======
->>>>>>> 5eee49e0d4ebe94e5c570a40b64d9b8e348bb131
 end
