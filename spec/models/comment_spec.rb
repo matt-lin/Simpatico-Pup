5417f706e27a9b9a7b20881daed7003dc2897bb7
Alex Ho <hogunghiu@msn.com>
2017-07-17 21:22:47 +0000

    Mege

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
-describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'validate comment length' do
+  @good_comment = FactoryGirl.create(:comment, :content => 'Testing', :pup_id => 1)
+
+  
+    it "should not raise" do
+        expect{@good_comment.comment_length}.to_not raise_error()
+    end
+  
+    # it "should raise error" do
+    #     expect{@bad_comment.comment_length}.to raise_error
+    # end
 end
