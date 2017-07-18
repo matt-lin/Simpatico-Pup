0d22681c31acdbe9ea6d6c3a3e6c91f7f44298cf
Hongzhan Yu <hyu596@berkeley.edu>
2017-07-18 23:44:43 +0000

    <Development> working on rspec

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -1,19 +1,23 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe PasswordResetsController do
+describe PasswordresetsController do
 
-#   describe "GET 'new'", :pending => true do
-#     it "returns http success" do
-#       get 'new'
-#       response.should be_success
-#     end
-#   end
+  describe "resetting password" do
+      before :each do
+          5.times.each do |i|
+             FactoryGirl.create(:user, email: "user#{i}@berkeley.edu", password: '12345678', password_confirmation: '12345678')
+          end
+      end
+      it "should start with proper page that asks for email" do
+          get :new
+          response.should render_template 'new'
+      end
+      it "should send an email if valid" do
+          User.should_receive(:find_by).with({:email => "user1@berkeley.edu"}).and_return(User.find(1))
+          post :create, {:password_reset => {:email => "user1@berkeley.edu"}}
+          response.should redirect_to root_path
+          open_email('user1@berkeley.edu')
+      end
+  end
 
-#   describe "GET 'edit'", :pending => true do
-#     it "returns http success" do
-#       get 'edit'
-#       response.should be_success
-#     end
-#   end
-
-# end
+end
