86f157c10878c7e8b380a574f3606dc4ba950d40
Alex Ho <hogunghiu@msn.com>
2017-07-19 01:27:39 +0000

    <maint> Remove comment tag in admin page

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -100,7 +100,11 @@ ActiveAdmin.setup do |config|
   # Admin comments are enabled by default.
   #
   # Default:
-  config.comments = true
+  config.comments = false
+  
+  # for iter 2
+  config.show_comments_in_menu = false
+  
   #
   # You can turn them on and off for any given namespace by using a
   # namespace config block.
