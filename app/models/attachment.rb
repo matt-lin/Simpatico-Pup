9b47762d6655960a9d7bbefa979d779383158511
Alex Ho <hogunghiu@msn.com>
2017-07-20 09:04:15 +0000

    <feat> Now can upload file to specific folder

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,5 @@
 class Attachment < ActiveRecord::Base
-    has_attached_file :document, :path => ":rails_root/:basename.:extension"
+    has_attached_file :document, :path => ":rails_root/app/assets/attachment/:basename.:extension"
     
     
    do_not_validate_attachment_file_type :document
