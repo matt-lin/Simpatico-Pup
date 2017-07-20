233d240f367ed173a4cd923afe73a03ffad10b5d
Alex Ho <hogunghiu@msn.com>
2017-07-20 12:19:10 +0000

    <maint> Deleted firmwareTable

--- db/migrate/20170720051626_create_attachments.rb
+++ db/migrate/20170720051626_create_attachments.rb
@@ -2,6 +2,7 @@ class CreateAttachments < ActiveRecord::Migration
   def change
     create_table :attachments do |t|
       t.attachment :document
+      t.belongs_to :selected_attachment, index: true
     end
   end
 end
