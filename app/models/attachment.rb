154867bbd8550f786f1bd92a142bea9f25dcce99
Alex Ho <hogunghiu@msn.com>
2017-07-21 08:49:54 +0000

    <feat> Increased security on file manager

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,9 @@
 class Attachment < ActiveRecord::Base
     belongs_to :selected_comment
-    has_attached_file :document
+    attachment_virtual_path = "/system/attachments/:rails_env/:hashed_path/:id/:style/:basename.:extension"
+    attachment_real_path = ":rails_root/public" + attachment_virtual_path
+
+    has_attached_file :document, :path => attachment_real_path, :url => attachment_virtual_path
     do_not_validate_attachment_file_type :document
     
     @@filenames = []
@@ -12,5 +15,4 @@ class Attachment < ActiveRecord::Base
     def self.get_files 
         return @@filenames
     end
-    
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,4 +5,16 @@ module Paperclip
       false
     end
   end
+  
+  Paperclip.interpolates :hashed_path do |attachment, style|
+  secret = 'YP#nb-kjGJpCF4_=ce2QYPt#!-VL33pKz8$aGN7CEuun-e8XeDb$3yVKDFXrSA7$+#wpDQfj^wR4tf^3FWbL4?G#taE^?!eE9Fswch_wNxE$G
+  VaM5QWcH49RpHutsv8-U%bcGGU9=-CfTqUe7=JHvqf@Qz$fPXFJ_wnDaecVMBdWC7J3hFnV4m7Sa3tWcntxhYaszYScc@YdAujx7gvu3@DsZXQJ*ZzyZHnM
+  T%n5%KxwZnQt=mrFP+H*RZPbWDmk'
+  hash = Digest::MD5.hexdigest("--#{attachment.class.name}--#{attachment.instance.id}--#{secret}--")
+  hash_path = ''
+  6.times { hash_path += '/' + hash.slice!(0..2) }
+  hash_path[1..24]
+end
+  
+  
 end
