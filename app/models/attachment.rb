56764547ed228de5888187b68c181af5038e9833
Alex Ho <hogunghiu@msn.com>
2017-07-23 03:01:14 +0000

    <bug> attempting to restrict assignment for attachment model

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,9 +1,10 @@
 class Attachment < ActiveRecord::Base
     belongs_to :selected_comment
+    attr_accessor :document
+    
     attachment_virtual_path = "/system/attachments/:rails_env/:hashed_path/:id/:style/:basename.:extension"
     attachment_real_path = ":rails_root/public" + attachment_virtual_path
 
     has_attached_file :document, :path => attachment_real_path, :url => attachment_virtual_path
     do_not_validate_attachment_file_type :document
-    
 end
