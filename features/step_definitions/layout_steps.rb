79f37aaa7c605923edf5be3799bb36a15c25e4da
Licong Wang <lcw@berkeley.edu>
2017-07-25 22:27:52 +0000

    maint: fixed more previous cucumber tests
    added new default layouts to step definition

--- features/step_definitions/layout_steps.rb
+++ features/step_definitions/layout_steps.rb
@@ -1,6 +1,7 @@
 #Iter 3-2
 Given (/^the default layout exist$/) do
   layouts = {
+    Site_Name: 'SimpaticoPup',
     Primary_Title: 'Better Way to Find a Great Dog',
     Secondary_Title: 'Find Dog Breeders Near You',
     Rate_Dog_Top: 'Rate your',
@@ -10,7 +11,13 @@ Given (/^the default layout exist$/) do
     Find_Breeder_Top: 'Find Ratings for a',
     Find_Breeder_Bottom: 'Breeder',
     Search_Breeder_Top: 'Search for a',
-    Search_Breeder_Bottom: 'Breeder'
+    Search_Breeder_Bottom: 'Breeder',
+    Blue_Bar_Top_Left: 'For More Information on Breed Health',
+    Blue_Bar_Top_Middle: 'Privacy Policy',
+    Blue_Bar_Top_Right: 'Contact Info',
+    Blue_Bar_Bottom_Left: 'Canine Health Information Center',
+    Blue_Bar_Bottom_Middle: 'Terms of Service',
+    Blue_Bar_Bottom_Right: 'LSandersDVM@gmail.com'
   }
   
   layouts.keys.each do |key|
