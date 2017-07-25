d8efe2761d5120997e20d7d4d0b7aa2bdacee6b8
Alex Ho <hogunghiu@msn.com>
2017-07-25 07:42:05 +0000

    feat :
    Feature is done, start finalizing cucumber

--- app/models/customize.rb
+++
@@ -1,8 +0,0 @@
-class Customize < ActiveRecord::Base
-    attr_accessible :welcome1, :welcome2
-    
-    @@texts = {
-     "welcome1" => "ha",
-     "welcome2" => "haha"
-    }
-end
--- db/migrate/20170725061323_create_customizes.rb
+++
@@ -1,9 +0,0 @@
-#Iter 3-2 Added table for customize (By Gung Hiu Ho, Licong Wang)
-class CreateCustomizes < ActiveRecord::Migration
-  def change
-    create_table :customizes do |t|
-      t.string :welcome1
-      t.string :welcome2
-    end
-  end
-end
