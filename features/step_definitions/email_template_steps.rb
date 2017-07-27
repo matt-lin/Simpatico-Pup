319945a4ed8ede160cbd9c2f81052dff62100644
jcjackchen <jcjack@berkeley.edu>
2017-07-27 04:08:39 +0000

    maint:  email template feature file
    DRIVER: zipei@berkeley.edu
    OBSERVER: jcjack@berkeley.edu

---
+++ features/step_definitions/email_template_steps.rb
@@ -0,0 +1,11 @@
+And (/^the following email_templates exist:$/) do |table|
+    pending
+end
+
+When (/^I check "([^"]*)","([^"]*)"$/) do |template|
+    pedning
+end
+
+And (/^I check send email_templates$/) do 
+    pending
+end
