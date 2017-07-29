708e51309e9dc2817c36f8ccddbe84d8a2a6f55d
jcjackchen <jcjack@berkeley.edu>
2017-07-29 01:10:27 +0000

    feat:  email template feature finalized design

--- app/admin/email_template.rb
+++ app/admin/email_template.rb
@@ -6,16 +6,43 @@ ActiveAdmin.register EmailTemplate do
         b.marked = false
         b.save
       end
+      
       batch_action_collection.find(id).each do |b|
         b.marked = true
         b.save
-        
       end
       redirect_to admin_newsletter_users_path
     else
       redirect_to collection_path, notice: "A email can only includ one Email Template"
     end
-    
+  end
+  
+  filter :title
+  filter :body
+  filter :created_at
+  filter :updated_at
+  
+  index do
+    selectable_column
+    column :title
+    column :body
+    actions
+  end
+  
+  sidebar :"How to use" do
+    h6 "Batch Actions"
+    ul do
+      li "Send Selected: Mark the selected email template and redirect to Newsletter Subscribers page to send out email template"
+      li "Delete Selected: Permanently remove the marked the template from the site"
+    end
+  end
+  
+  form do |f|
+    f.inputs "New Email Template" do
+      f.input :title
+      f.input :body
+    end
+    f.actions
   end
   
 end
