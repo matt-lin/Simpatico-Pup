75e59535b9999578c91b9c37c7e22a79113b0838
Licong Wang <lcw@berkeley.edu>
2017-07-29 03:29:03 +0000

    impr:  moved codes from controller to applicaiton helper
    also fixed secondary title text
    DRIVER: lcw@berkeley.edu
    OBSERVER: hogunghiu@msn.com

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,53 @@
 # Iter 3-2 Customize the layout (By Gung Hiu Ho, Licong Wang)
 module ApplicationHelper
   def titles
-    return  Customize.find_by_name("Site_Name").content
+    return Customize.find_by_name("Site_Name").content
   end
   
+  def primary_title 
+    return Customize.find_by_name("Primary_Title").content
+  end
+  
+  def secondary_title
+    return Customize.find_by_name("Secondary_Title").content
+  end
+  
+  def rate_dog_top
+    return Customize.find_by_name("Rate_Dog_Top").content
+  end
+  
+  def rate_Dog_bottom
+    return Customize.find_by_name("Rate_Dog_Bottom").content
+  end
+  
+  def find_breed_top
+    return Customize.find_by_name("Find_Breed_Top").content
+  end
+  
+  def find_breed_bottom
+    return Customize.find_by_name("Find_Breed_Bottom").content
+  end
+  
+  def find_breeder_top
+    return Customize.find_by_name("Find_Breeder_Top").content
+  end
+  
+  def find_breeder_bottom
+    return Customize.find_by_name("Find_Breeder_Bottom").content
+  end
+  
+  def search_breeder_top
+    return Customize.find_by_name("Search_Breeder_Top").content
+  end
+  
+  def search_breeder_bottom
+    return Customize.find_by_name("Search_Breeder_Bottom").content
+  end
+  
+  def comment_title
+    return Customize.find_by_name("Comment_Title").content
+  end
+    
   def top_left
     return Customize.find_by_name("Blue_Bar_Top_Left").content
   end
