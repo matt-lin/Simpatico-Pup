b27a1b4f543b0ad6f4b716324df52389d7834def
Alex Ho <hogunghiu@msn.com>
2017-07-30 23:42:19 +0000

    impr:  Restrict mass assignment when acessing model comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,14 @@
 class Comment < ActiveRecord::Base
 
   belongs_to :pup
-
+  
+  #Restrict mass assignment
+  attr_accessible :content
+  attr_accessible :breed
+  attr_accessible :pup_id
+  attr_accessible :created_at
+  attr_accessible :updated_at
+  
   attr_accessor :breed, :breeder
 
   validate :comment_length
