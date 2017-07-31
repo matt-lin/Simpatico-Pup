998b68f2846789ed12c0adb299eeecd02b9123ef
Jack Chen <jcjack@berkeley.edu>
2017-07-31 06:13:32 +0000

    recover

---
+++ app/controllers/account_activations_controller.rb
@@ -0,0 +1,20 @@
+# Iter 2-2 account actication (by Zipei Wang and Jack Chen)
+class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated?
+      user.activate
+      user.save!
+      
+      sign_in(user,scope:user)
+      flash[:success] = "Congratulations! Your account has been activated!"
+      UserMailer.welcome(user).deliver_now
+      redirect_to root_url
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
+end
+#End for Iter 2-2
