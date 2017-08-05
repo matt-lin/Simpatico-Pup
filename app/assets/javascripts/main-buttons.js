61c8acb285ade0b6fa1ecff8e094ff37d8837b12
Hongzhan Yu <hyu596@berkeley.edu>
2017-08-05 02:11:59 +0000

    fix:

--- app/assets/javascripts/main-buttons.js
+++ app/assets/javascripts/main-buttons.js
@@ -21,13 +21,15 @@ var MainButtons = {
             $('.box-c').toggleClass("hidden");
         });
 
-        $('#to_goal_button').click( function() {
+        $('#to_goal_button').click( function(e) {
+            e.preventDefault();
             $('html,body').animate({
               scrollTop: $(".page_divider1").offset().top-50},
             'slow');
         });
         
-        $('#to_inline_button_button').click( function() {
+        $('#to_inline_button_button').click( function(e) {
+            e.preventDefault();
             $('html,body').animate({
               scrollTop: $(".page_divider2").offset().top-100},
             'slow');
@@ -87,5 +89,7 @@ var MainButtons = {
 
 $(document).ready(function () {
     $(MainButtons.setup);
-    MainButtons.slider();
+    if(window.location.pathname === '/') {
+        MainButtons.slider();
+    }
 });
