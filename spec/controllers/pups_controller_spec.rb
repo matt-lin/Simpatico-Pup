a4ffe70cc34aefed3fd6667c128968bcaa1892ba
Gilbert Lo <lhygilbert@berkeley.edu>
2017-08-06 05:23:28 +0000

    maint:  adjust test for new design of pup page

--- spec/controllers/pups_controller_spec.rb
+++ spec/controllers/pups_controller_spec.rb
@@ -286,11 +286,7 @@ describe PupsController do
       session[:pup_name] = "Doggie"
       get :dog_breed, {:pup=>{:years=>"",:months=>"3"}}
       expect(response).to redirect_to dog_how_long_path(:pup=>{:name=>"Doggie"})
-      expect(flash[:modal]).to eq("To keep our database as accurate as possible,
-we are collecting information only for dogs that have been residing 
-in their current home for six months or more. Please come back to our 
-site and rate your dog after s/he has lived 
-with you for a minimum of six months. Thank you.")
+      expect(flash[:modal]).not_to be_empty
       expect(session[:step2]).to be_false
     end
     
