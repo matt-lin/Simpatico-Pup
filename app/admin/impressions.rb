992727655c219528571457557607decbe87bb91c
Alex Ho <hogunghiu@msn.com>
2017-08-06 21:15:25 +0000

    feat:  implemented all the basic functionally
    DRIVER: hogunghiu@msn.com
    OBSERVER: lcw@berkeley.edu

--- app/admin/impressions.rb
+++ app/admin/impressions.rb
@@ -1,7 +1,10 @@
 ActiveAdmin.register Impression do
     menu :label => 'Site Analysis'
+    actions :index
     config.batch_actions = false
-    
+    config.filters = false
+    config.paginate = false
+
     index do
         panel "User Statistics" do
             columns do
@@ -10,7 +13,7 @@ ActiveAdmin.register Impression do
                     width: "500px", height: "300px", xtitle: "Date", ytitle: "Population", title: "User"  
                 end
                 column do
-                    area_chart NewsletterUser.group_by_day(:id).count, library: {animation: {duration: 1150, easing: 'easeOutQuad' }},
+                    area_chart NewsletterUser.group_by_day(:created_at).count, library: {animation: {duration: 1150, easing: 'easeOutQuad' }},
                     width: "500px", height: "300px", xtitle: "Date", ytitle: "Population", title: "Subscriber Trending", animation: "true"
                 end
             end
@@ -21,12 +24,55 @@ ActiveAdmin.register Impression do
                     line_chart Pup.group_by_day(:created_at).count, library: {animation: {duration: 1050, easing: 'easeOutQuad' }},
                     width: "500px", height: "300px", xtitle: "Date", ytitle: "Population", title: "Pup Creation", animation: "true"
                 end
+                column do
+                    bar_chart Pup.group(:year).count, library: {animation: {duration: 1050, easing: 'easeOutQuad' }},
+                    width: "500px", height: "300px", xtitle: "Population", ytitle: "Year", title: "Age saturation", animation: "true"
+                end
+            end
+            columns do
+                column do
+                    scatter_chart Pup.group(:breeder_responsibility).count, library: {animation: {duration: 1050, easing: 'easeOutQuad' }},
+                    width: "500px", height: "300px", xtitle: "Date", ytitle: "Population", title: "Breeder Responsibility", animation: "true"
+                end
+                column do
+                    scatter_chart Pup.group(:overall_health).count, library: {animation: {duration: 1050, easing: 'easeOutQuad' }},
+                    width: "500px", height: "300px", xtitle: "Population", ytitle: "Year", title: "Overall Health", animation: "true"
+                end
+            end
+            columns do
+                column do
+                    scatter_chart Pup.group(:trainability).count, library: {animation: {duration: 1050, easing: 'easeOutQuad' }},
+                    width: "500px", height: "300px", xtitle: "Date", ytitle: "Population", title: "Trainability", animation: "true"
+                end
+                column do
+                    scatter_chart Pup.group(:social_behavior).count, library: {animation: {duration: 1050, easing: 'easeOutQuad' }},
+                    width: "500px", height: "300px", xtitle: "Population", ytitle: "Year", title: "Social Behavior", animation: "true"
+                end
+            end
+            columns do
+                column do
+                    scatter_chart Pup.group(:energy_level).count, library: {animation: {duration: 1050, easing: 'easeOutQuad' }},
+                    width: "500px", height: "300px", xtitle: "Date", ytitle: "Population", title: "Energy level", animation: "true"
+                end
+                column do
+                    scatter_chart Pup.group(:simpatico_rating).count, library: {animation: {duration: 1050, easing: 'easeOutQuad' }},
+                    width: "500px", height: "300px", xtitle: "Population", ytitle: "Year", title: "Overall rating", animation: "true"
+                end
             end
         end
         panel "Breeder Statistics" do
             columns do
                 column do
-                    line_chart Breeder.group_by_day(:id).count, library: {animation: {duration: 1100, easing: 'easeOutQuad' }},
+                    area_chart ({
+                        0.day.ago => Impression.where(controller_name: "breeders").where(action_name: "create").where(created_at: 0.day.ago.all_day).length,
+                        1.day.ago => Impression.where(controller_name: "breeders").where(action_name: "create").where(created_at: 1.day.ago.all_day).length,
+                        2.day.ago => Impression.where(controller_name: "breeders").where(action_name: "create").where(created_at: 2.day.ago.all_day).length,
+                        3.day.ago => Impression.where(controller_name: "breeders").where(action_name: "create").where(created_at: 3.day.ago.all_day).length,
+                        4.day.ago => Impression.where(controller_name: "breeders").where(action_name: "create").where(created_at: 4.day.ago.all_day).length,
+                        5.day.ago => Impression.where(controller_name: "breeders").where(action_name: "create").where(created_at: 5.day.ago.all_day).length,
+                        6.day.ago => Impression.where(controller_name: "breeders").where(action_name: "create").where(created_at: 6.day.ago.all_day).length,
+                        7.day.ago => Impression.where(controller_name: "breeders").where(action_name: "create").where(created_at: 7.day.ago.all_day).length,
+                        }), library: {animation: {duration: 1100, easing: 'easeOutQuad' }},
                     width: "500px", height: "300px", xtitle: "Date", ytitle: "Population", title: "Breeder Creation", animation: "true"
                 end
                 column do
