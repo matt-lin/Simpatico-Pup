bafc572e85803806f22962107e6ddfa105bbc53f
Licong Wang <lcw@berkeley.edu>
2017-09-24 04:19:49 +0000

    fix: removed unnecessary rows in site preference

--- app/models/customize.rb
+++ app/models/customize.rb
@@ -1,3 +1,17 @@
 class Customize < ActiveRecord::Base
     attr_accessible :name, :content, :location
+    
+    @@excluded = ['Title_Button', 'Primary_title', 'Secondary_Title', 'Transition_Button', 'Function_Title']
+    @@deleted = false
+    
+    def Customize.delete
+        if !@@deleted
+            @@excluded.each do |name|
+                if !Customize.find_by_name(name).nil?
+                    Customize.find_by_name(name).destroy
+                end
+            end
+            @@deleted = true
+        end
+    end
 end
