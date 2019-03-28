6ada3a3ba334df3d2b838fda25934f61020b8bda
mattlin <matthewytlin@berkeley.edu>
2019-03-28 20:10:54 +0800

    Fixed user activation_digest to correctly authenticate the user

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,7 +3,8 @@ class AccountActivationsController < ApplicationController
 
   def edit
     user = User.find_by(email: params[:email])
-    if user && !user.activated? && params[:id] == user.activation_digest
+    auth = BCrypt::Password.new(user.activation_digest)
+    if user && !user.activated? && auth == params[:id]
       user.activate
       user.save!
       sign_in(user,scope:user)
@@ -18,7 +19,7 @@ class AccountActivationsController < ApplicationController
 
   def send_mail
     user = User.find_by_email(params[:email])
-    
+
     if user == nil
       redirect_to root_url
       return
@@ -32,4 +33,3 @@ class AccountActivationsController < ApplicationController
   end
 end
 #End for Iter 2-2
-
