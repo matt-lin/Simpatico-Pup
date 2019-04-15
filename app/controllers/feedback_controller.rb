b9f8f567bb61b4f79fb45e58cab59cb960aaadb5
Eric Kong <eric.kong98@gmail.com>
2019-04-15 13:16:55 -0700

    initial create-controller for feedback

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,13 +1,34 @@
 class FeedbackController < ApplicationController
-  def index
-  end
 
   def new
+    # Auto renders 'new' template
   end
 
   def create
+    if !params.has_key?('feedback')
+      redirect_to feedback_path and return
+    end
+    open_resp = params[:feedback][:open_response]
+    rate_dog = params[:feedback][:rate_dog]
+    breed_rating = params[:feedback][:breed_rating]
+    breeder_rating = params[:feedback][:breeder_rating]
+    forum = params[:feedback][:forum]
+
+    # All scale ratings mandatory
+    if rate_dog.empty? || breed_rating.empty? || breeder_rating.empty? || forum.empty?
+      flash[:notice] = "Please complete the form."
+      redirect_to feedback_path and return
+    end
+
+    @feedback = Feedback.new(params[:feedback])
+
+    if @feedback.save
+      flash[:message] = "Thanks for your feedback!"
+      redirect_to feedback_path, alert: "Feedback submitted successfully."
+    else
+      flash[:message] = "Oops, something went wrong!"
+      redirect_to feedback_path, alert: "Error submitting feedback."
+    end
   end
 
-  def show
-  end
 end
