2a923ad506d7d30d7965bb2bd0410ba3cb577920
Eric Kong <eric.kong98@gmail.com>
2019-04-15 14:19:52 -0700

    Functionality working for feedback submission

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -10,26 +10,33 @@ class FeedbackController < ApplicationController
       redirect_to feedback_path and return
     end
     open_resp = params[:feedback][:open_response]
-    rate_dog = params[:feedback][:rate_dog]
-    breed_rating = params[:feedback][:breed_rating]
-    breeder_rating = params[:feedback][:breeder_rating]
-    search_breeder = params[:feedback][:search_breeder]
-    forum = params[:feedback][:forum]
+    rate_dog = params[:feedback][:usability_rateyourdog]
+    breed_rating = params[:feedback][:usability_findratingbreed]
+    breeder_rating = params[:feedback][:usability_findratingbreeder]
+    search_breeder = params[:feedback][:usability_searchbreeder]
+    forum = params[:feedback][:usability_forum]
 
     # All scale ratings mandatory
-    if rate_dog == '0' || breed_rating == '0' || breeder_rating == '0' || forum == '0'
+    if rate_dog == '0' || breed_rating == '0' || breeder_rating == '0' || search_breeder == '0' || forum == '0'
       flash[:notice] = "Please complete the form."
       redirect_to feedback_path and return
     end
 
-    @feedback = Feedback.new(params[:feedback])
+    @feedback = Feedback.create!(
+      :open_response => open_resp,
+      :rate_dog => rate_dog,
+      :breed_rating => breed_rating,
+      :breeder_rating => breeder_rating,
+      :search_breeder => search_breeder,
+      :forum => forum
+    )
 
     if @feedback.save
-      flash[:message] = "Thanks for your feedback!"
-      redirect_to feedback_path, alert: "Feedback submitted successfully."
+      flash[:notice] = "Thanks for your feedback!"
+      redirect_to feedback_path
     else
-      flash[:message] = "Oops, something went wrong!"
-      redirect_to feedback_path, alert: "Error submitting feedback."
+      flash[:notice] = "Oops, something went wrong!"
+      redirect_to feedback_path
     end
   end
 
