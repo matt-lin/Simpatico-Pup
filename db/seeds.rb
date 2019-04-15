dcb79f5ab28b47d28f7ce68defd829094eb5711f
shuyinxu <xu.shuyin98@gmail.com>
2019-04-15 03:10:45 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -337,4 +337,4 @@ texts.each do |text|
   Text.create!(text)
 end
 
-AdminUser.create!(:email => 'admin@berkeley.edu', :password => 'password', :password_confirmation => 'password')
+#AdminUser.create!(:email => 'admin@berkeley.edu', :password => 'password', :password_confirmation => 'password')
