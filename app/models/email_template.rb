cfc9038b901564a6d9e19ea06d63543ea58bb197
Eric Kong <eric.kong98@gmail.com>
2019-04-29 13:41:26 -0700

    rspec tests for email template model

--- app/models/email_template.rb
+++ app/models/email_template.rb
@@ -5,4 +5,23 @@ class EmailTemplate < ActiveRecord::Base
     attr_accessible :created_at
     attr_accessible :updated_at
     attr_accessible :marked
+
+    #private
+    def EmailTemplate.all_emails
+      return EmailTemplate.select('title').map{ |e| e.title }.to_a
+    end
+
+    def EmailTemplate.is_valid_email(title)
+      EmailTemplate.all_emails.include? title
+    end
+
+    def EmailTemplate.find_by_email_field(field, text)
+      query = field + " = ?"
+      result = EmailTemplate.where(query, text).first
+
+      if !result
+        return nil
+      end
+      return result
+    end
 end
--- spec/models/email_template_spec.rb
+++ spec/models/email_template_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
 
 describe EmailTemplate do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @email1 = FactoryGirl.create(:email_template, :title => "Hello!", :body => "Check out our new updates!")
+    @email2 = FactoryGirl.create(:email_template, :title => "Good Morning", :body => "Give us some feedback")
+  end
+
+  describe 'all_email_templates' do
+    it 'return all email templates as array' do
+      res = [@email1.title, @email2.title]
+      expect(EmailTemplate.all_emails).to eq res
+    end
+  end
+
+  describe 'is_valid_email' do
+    it 'return false for not valid email' do
+      expect(EmailTemplate.is_valid_email('random')).to eq false
+      expect(EmailTemplate.is_valid_email('another one')).to eq false
+    end
+
+    it 'return true for valid email' do
+      expect(EmailTemplate.is_valid_email('Hello!')).to eq true
+      expect(EmailTemplate.is_valid_email('Good Morning')).to eq true
+    end
+  end
+
+  describe 'find_by_title' do
+    it 'return nil for no matching email' do
+      expect(EmailTemplate.find_by_email_field('title', 'Nothing')).to eq nil
+    end
+
+    it 'return email matching title' do
+      expect(EmailTemplate.find_by_email_field('title', 'Hello!')).to eq @email1
+    end
+  end
+
+  describe 'find_by_body' do
+    it 'return nil for no matching email' do
+      expect(EmailTemplate.find_by_email_field('body', 'more random things')).to eq nil
+    end
+
+    it 'return email matching body' do
+      expect(EmailTemplate.find_by_email_field('body', 'Give us some feedback')).to eq @email2
+    end
+  end
+
 end
