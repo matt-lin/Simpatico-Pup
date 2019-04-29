9fbd305c4a48f3f89c66063093ac5723ae70819b
mattlin <matthewytlin@berkeley.edu>
2019-04-28 22:20:53 -0700

    Added bio field to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190416001403) do
+ActiveRecord::Schema.define(version: 20190429022050) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "resource_id",   null: false
@@ -435,6 +435,7 @@ ActiveRecord::Schema.define(version: 20190416001403) do
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.string   "avatar"
+    t.string   "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
