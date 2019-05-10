fb450824f07d04054322bdce1a97a52ff11d64a0
Shuyin <xshuyin@berkeley.edu>
2019-05-10 14:16:54 -0700

    code climate fix

--- app/assets/javascripts/main-buttons.js
+++ app/assets/javascripts/main-buttons.js
@@ -27,12 +27,16 @@ var MainButtons = {
               scrollTop: dest},'slow');
         }
 
+        function dest_assigner(class_name, offset_amount){
+            return $(class_name).offset().top-offset_amount;
+        }
+
         $('#to_goal_button').click( function(e) {
-            pageSlide(e, $(".page_divider1").offset().top-50);
+            pageSlide(e, dest_assigner(".page_divider1",50));
         });
         
         $('#to_inline_button_button').click( function(e) {
-            pageSlide(e, $(".page_divider2").offset().top-100);
+            pageSlide(e, dest_assigner(".page_divider2", 100));
         });
     },
     slider: function() {
@@ -82,6 +86,9 @@ var MainButtons = {
         }
     }
 };
+
+function showSlides(n) {
+}
 
 
 $(document).ready(function () {
